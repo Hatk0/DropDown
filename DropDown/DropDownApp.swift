1ef659f8c16a3712b92a81db288a18b4471b0a8c
Dmitry Yastrebov <stewielime2018@gmail.com>
2024-12-13 16:44:45 +0500

    Initial Commit

---
+++ DropDown/DropDownApp.swift
@@ -0,0 +1,17 @@
+//
+//  DropDownApp.swift
+//  DropDown
+//
+//  Created by Dmitry Yastrebov on 13.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct DropDownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
